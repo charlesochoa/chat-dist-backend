c4a52c1db6ea195f2818e2f82fa5445a910205b6
cochoa_hiberus <cochoa@hiberus.com>
2020-03-25 16:28:20 +0100

    Comunicación realizada entre Backend y Auxiliar como si fuesen emisor y receptor respectivamente

--- src/main/java/chatdist/backend/BackendApplication.java
+++ src/main/java/chatdist/backend/BackendApplication.java
@@ -6,10 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.concurrent.TimeoutException;
+
 
 @SpringBootApplication
 public class BackendApplication {
 
+	private final static String QUEUE_NAME = "hola";
 	private static String userName = "byfntbvj";
 	private static  String password = "2x_P1v83EjPv9MOr9ZEycnWq-ct7MDHE";
 	private static String vHost  = "byfntbvj";
@@ -18,7 +23,7 @@ public class BackendApplication {
 	private static String uri = "amqp://byfntbvj:2x_P1v83EjPv9MOr9ZEycnWq-ct7MDHE@kangaroo.rmq.cloudamqp.com/byfntbvj";
 
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 
 		ConnectionFactory factory = new ConnectionFactory();
 		SpringApplication.run(BackendApplication.class, args);
@@ -30,14 +35,39 @@ public class BackendApplication {
 		try {
 
 			factory.setUri(uri);
-			Connection conn = factory.newConnection();
-			Channel channel = conn.createChannel();
-			Thread.sleep(10000);
-			channel.close();
-			conn.close();
 		} catch (Exception e){
 			System.out.println(e);
+			System.exit(-1);
 		}
+
+		Connection conn = factory.newConnection();
+		Channel channel = conn.createChannel();
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+		int messageNumber;
+		boolean end = false;
+
+		do {
+			System.out.println("Escribe un número y pulsa <Enter> para enviarlo. El 0 para finalizar.");
+			Scanner sc = new Scanner(System.in);
+			messageNumber = sc.nextInt();
+			if (messageNumber == 0) {
+				end = true;
+			} else {
+				String message = "Mensaje: " + messageNumber;
+				// En el modelo de mensajería de RabbitMQ los productores nunca mandan mensajes
+				// directamente a colas, siempre los publican a un exchange (centralita) que
+				// los enruta a colas (por criterios definidos según el tipo de exchange).
+				// En este caso, el string vacío (primer parámetro) identifica el "default" o
+				// "nameless" exchange: los mensajes se enrutan a la cola indicad por
+				// routingKey (segundo parámetro) si existe.
+				channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+				System.out.println(" [x] Enviado '" + message + "'");
+			}
+		} while (!end);
+
+		channel.close();
+		conn.close();
 	}
 
 }
