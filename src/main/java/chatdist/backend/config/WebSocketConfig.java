018328e5761afd565ec1f364c1820b65e3e945b5
cochoa_hiberus <cochoa@hiberus.com>
2020-05-02 16:31:23 +0200

    some bugs resolved. no configuration for rabbitmq

--- src/main/java/chatdist/backend/config/WebSocketConfig.java
+++ src/main/java/chatdist/backend/config/WebSocketConfig.java
@@ -15,16 +15,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableStompBrokerRelay("/topic")
-                .setRelayHost("kangaroo.rmq.cloudamqp.com")
-                .setRelayPort(61613)
-                .setClientLogin("byfntbvj")
-                .setClientPasscode("2x_P1v83EjPv9MOr9ZEycnWq-ct7MDHE");
+        registry.enableSimpleBroker("/topic");
     }
 }
