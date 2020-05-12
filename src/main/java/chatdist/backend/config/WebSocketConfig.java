bc53907fb86958eee16a405b3df756fed1fe9155
Gabriel Alvarez <gabrielaar11@gmail.com>
2020-05-12 16:22:00 +0200

    Spring security and web sockets integration

--- src/main/java/chatdist/backend/config/WebSocketConfig.java
+++ src/main/java/chatdist/backend/config/WebSocketConfig.java
@@ -1,16 +1,57 @@
 package chatdist.backend.config;
 
 
+import chatdist.backend.filter.JWTConstants;
+import chatdist.backend.model.User;
+import chatdist.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.socket.config.annotation.*;
 
+import java.security.Principal;
+import java.util.Optional;
 
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    @Autowired
+    UserRepository userRepository;
+
+//    @Override
+//    public void configureClientInboundChannel(ChannelRegistration registration) {
+//        registration.interceptors(new ChannelInterceptor() {
+//            @Override
+//            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+//
+//                StompHeaderAccessor accessor =
+//                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+//
+//                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+//                    String authToken = accessor.getFirstNativeHeader("Authorization");
+//                    System.out.println(authToken);
+//                    String username = JWTConstants.parseToken(authToken) ; // access authentication header(s)
+//                    Optional<User> optionalUser = userRepository.findByUsername(username);
+//                    if (optionalUser.isPresent()) {
+//                        accessor.setUser(optionalUser.get());
+//                    };
+//                }
+//
+//                return message;
+//            }
+//        });
+//    }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
